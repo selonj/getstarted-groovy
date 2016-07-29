d8629908a49ef213e4bf3cd332aa145d6cbf1dc8
selonj <selonj@126.com>
2016-07-30 02:46:29 +0800

    syntax/booleans

---
+++ src/test/groovy/com/holi/syntax/BooleansTest.groovy
@@ -0,0 +1,13 @@
+package com.holi.syntax
+
+import org.junit.Test
+
+/**
+ * Created by selonj on 16-7-30.
+ */
+class BooleansTest {
+  @Test void "literal"() {
+    assert true;
+    assert !false;
+  }
+}
